0d9d9195bf75460d2a3f5e3251cf58b8d28c5f6c
jong-jin624 <jongjin1004baek@gmail.com>
2025-12-03 16:53:33 +0900

    js 데이터 export import

---
+++ src/components/Export01.js
@@ -0,0 +1,17 @@
+import menus from "../data/mydata01";
+import { menu4, menu5, menu6 } from "../data/mydata02";
+
+function Export01(){
+    return (
+        <div>
+            <h1>Export01</h1>
+            <h2>{menus}</h2>
+            <h2>{menu4}</h2>
+            <h2>{menu5}</h2>
+            <h2>{menu6}</h2>
+
+        </div>
+    )
+}
+
+export default Export01;
---
+++ src/data/mydata01.js
@@ -0,0 +1,7 @@
+let menu1 = '김밥';
+let menu2 = '돈까스';
+let menu3 = '라면';
+
+let menus = menu1 + menu2 + menu3;
+
+export default menus;
